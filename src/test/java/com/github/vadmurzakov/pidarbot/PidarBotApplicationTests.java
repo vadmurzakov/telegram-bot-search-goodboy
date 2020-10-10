774c55ad3a1dd7c4e5c31b621db71c6f8155a472
Мурзаков Вадим Викторович <murzakov.vadim@otr.ru>
2020-10-10 13:30:04 +0300

    [GH-1] initial repo

---
+++ src/test/java/com/github/vadmurzakov/pidarbot/PidarBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vadmurzakov.pidarbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PidarBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
