d18dfb362d4e649cd4b2bf86c375e80dab283d97
vmurzakov <vadmurzakov@gmail.com>
2020-10-11 02:02:27 +0300

    [GH-2] replace TelegramClient.java on dependency java-telegram-bot-api

--- src/main/java/com/github/vadmurzakov/pidarbot/domain/response/Model.java
+++
@@ -1,4 +0,0 @@
-package com.github.vadmurzakov.pidarbot.domain.response;
-
-public abstract class Model {
-}
--- src/main/java/com/github/vadmurzakov/pidarbot/domain/response/ResponseParameters.java
+++
@@ -1,9 +0,0 @@
-package com.github.vadmurzakov.pidarbot.domain.response;
-
-import lombok.Data;
-
-@Data
-public class ResponseParameters {
-    private Long migrateToChatId;
-    private Long retryAfter;
-}
